e72411c8d3a0013d4bcc4559150570de956c47ca
sahir <156174156+sahiee-dev@users.noreply.github.com>
2024-08-21 11:11:20 +0530

    Update fontawsome.js

--- src/fontawsome.js
+++ src/fontawsome.js
@@ -1,6 +1,6 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faBars, faPhone} from '@fortawesome/free-solid-svg-icons'; 
 import { faGithub, faLinkedin, faReact, faNode, faJs, faPython, faHtml5, faCss3Alt, faGitAlt} from '@fortawesome/free-brands-svg-icons';
-import { faEnvelope, faDatabase, faC } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope, faDatabase, faC ,faMoon } from '@fortawesome/free-solid-svg-icons';
 
 library.add(faBars, faGithub, faLinkedin, faEnvelope, faReact, faNode, faJs, faPython,faC, faDatabase, faHtml5, faCss3Alt, faGitAlt, faPhone, faBars);
